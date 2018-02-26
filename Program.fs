eb5fa60675ca4b1e80c853c3acf6ee15c4261144
Oomusou <oomusou@gmail.com>
2018-02-26 10:55:27 +0800

    By function

--- Program.fs
+++ Program.fs
@@ -1,8 +1,19 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿open System
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    let numbers = [1; 2; 3; 4; 5]
+
+    let isOdd x = x % 2 <> 0
+    let square x = x * x
+    let addOne x = x + 1
+    
+    let func values =
+        let odds = List.filter isOdd values
+        let squares = List.map square odds
+        let result = List.map addOne squares
+        result
+        
+    printfn "%A" (func numbers)
+    
+    0
